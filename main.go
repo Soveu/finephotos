4a855d2ff9887531a9f740d464931d0715dda657
Soveu <panrosolek@gmail.com>
2018-08-27 15:41:19 +0200

    finished the reddit api part

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"fmt"
+	"os"
 
 	"github.com/turnage/graw/reddit"
 )
@@ -9,17 +10,37 @@ import(
 func main(){
 	bot, err := reddit.NewBotFromAgentFile("useragent.txt", 0)
 	if err != nil {
-		fmt.Println("Failed to create bot: ", err)
+		//fmt.Println("Failed to create bot: ", err)
+		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 
-	posts, err := bot.Listing("/r/Art", "")
-	if err != nil {
-		fmt.Println("Failed to fetch /r/Art: ", err)
-		return
-	}
-
-	for _, post := range posts.Posts[:5] {
-		fmt.Printf("[%s] posted [%s] - [%s]\n", post.Author, post.Title, post.URL)
+	subreddits := []string{"Art", "EarthPorn"}
+	
+	for _, sub := range subreddits {
+		lp_file, err := os.OpenFile(fmt.Sprintf("lastpost_%s", sub), os.O_RDWR | os.O_CREATE, os.FileMode(0666))
+		lastpost := make([]byte, 9)
+		_, err = lp_file.Read(lastpost)
+	
+		art, err := bot.Listing(fmt.Sprintf("/r/%s", sub), string(lastpost))
+		if err != nil {
+			//fmt.Println("Failed to fetch /r/Art: ", err)
+			fmt.Fprintln(os.Stderr, "Failed to fetch ", sub, err)
+			return
+		}
+		
+		if len(art.Posts) > 0 {
+			for _, post := range art.Posts {
+				fmt.Println(post.URL)
+			}
+	
+			_, err = lp_file.WriteAt([]byte(art.Posts[0].Name), 0)
+			if err != nil {
+	//			fmt.Println(err)
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+		err = lp_file.Close()
 	}
 }
+
